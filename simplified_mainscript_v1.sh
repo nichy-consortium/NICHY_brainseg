40af6a2f5d9be530af7a42971b6c69a9ffe1b34e
NICHY consortium <e.vanheese@amsterdamumc.nl>
2022-08-31 16:19:14 -0400

    Create simplified_mainscript_v1.sh

---
+++ simplified_mainscript_v1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# simplified mainscript v1
+# E. van Heese - 31/08/2022
+
+do
+recon-all -subjid SUBJECTS_DIR/sub1_T1w -i SUBJECTS_DIR/sub1_T1w.nii.gz -all -notify log_done.txt 
+done
